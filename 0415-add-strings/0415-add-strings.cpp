0b2926554e4bb775f4ea2e1ef0cddb5ed13ec15e
Dhruv-krishn-a <150180757+Dhruv-krishn-a@users.noreply.github.com>
2024-07-22 09:26:00 +0530

    Time: 6 ms (36.86%), Space: 8.5 MB (48.44%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-string addRE(string num1, int p1 , string num2 , int p2 , int carry=0){
-    //Base case 
-    if(p1<0 && p2<0){
-        if(carry!=0){
-            return string(1, carry +'0');
-        }
-        return "";
-    }
-    //Processing 
-    int n1= (p1>=0 ? num1[p1] : '0') - '0';
-    int n2= (p2>=0 ? num2[p2] : '0') - '0';
-    int carrysum = n1+n2+carry;
-    int digit =carrysum%10;
-    carry = carrysum/10;
-    string ans = "";
-    ans.push_back(digit + '0');
-    
-    ans += addRE(num1 , p1-1 ,num2 ,p2-1 , carry);
-    return ans;
-}
     string addStrings(string num1, string num2) {
-        string ans = addRE(num1 , num1.size()-1,num2,num2.size()-1);
-        reverse(ans.begin(), ans.end());
-        return ans;
+        string res = "";
+    int i = num1.length() - 1;
+    int j = num2.length() - 1;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry != 0) {
+        int ival = i >= 0 ? num1[i] - '0' : 0;
+        int jval = j >= 0 ? num2[j] - '0' : 0;
+        i--;
+        j--;
+        int sum = ival + jval + carry;
+        res += (sum % 10) + '0';
+        carry = sum / 10;
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
     }
 };
